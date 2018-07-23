f3670b08f0feb8783ccfc4340d84ce0800fe5e0e
Felix König <felix0koenig@gmail.com>
2018-07-23 17:24:22 +0200

    Object listener node WIP.

---
+++ mrpt_map/include/object_listener_node.h
@@ -0,0 +1,36 @@
+#ifndef OBJECT_LISTENER_NODE_H
+#define OBJECT_LISTENER_NODE_H
+
+#include <ros/ros.h>
+#include <nav_msgs/OccupancyGrid.h>
+#include <tuw_object_msgs/ObjectDetection.h>
+#include <boost/filesystem.hpp>
+#include <mrpt/maps/CMultiMetricMap.h>
+#include <fstream>
+
+/**
+ * This class listens to objectdetections and inserts them into mrpt maps.
+ * Furthermore, it also listens to nav_msgs::OccupancyGridMap messages and converts those maps to mrpt maps
+ * @brief The ObjectListener class
+ */
+
+class ObjectListener
+{
+  public:
+    ObjectListener(ros::NodeHandle &n);
+    ~ObjectListener();
+
+    void init();
+    void callbackMap(nav_msgs::OccupancyGrid &_msg);
+    void callbackObjectDetections(tuw_object_msgs::ObjectDetection &_msg);
+
+  private:
+    ros::NodeHandle n_;
+    ros::NodeHandle n_param_{"~"};
+    ros::Subscriber sub_map_;
+    ros::Subscriber sub_object_detections_;
+    std::ofstream map_file_;
+    boost::shared_ptr<mrpt::maps::CMultiMetricMap> metric_map_;
+};
+
+#endif // OBJECT_LISTENER_NODE_H
---
+++ mrpt_map/src/object_listener_node.cpp
@@ -0,0 +1,55 @@
+#include <object_listener_node.h>
+#include <mrpt_bridge/map.h>
+#include <mrpt/maps/CBeaconMap.h>
+
+ObjectListener::ObjectListener(ros::NodeHandle& n) : n_(n)
+{}
+
+void ObjectListener::init()
+{
+  std::string ini_file;
+  std::string map_file;
+//  n_param_.param<std::string>("ini_file", ini_file, "map.ini");
+//  ROS_INFO("ini_file: %s", ini_file.c_str());
+  n_param_.param<std::string>("map_file", map_file, "map.simplemap");
+
+  ROS_INFO("map_file: %s", map_file.c_str());
+
+//  if (ini_file == std::string(""))
+//  {
+//    ROS_ERROR("specify a ini file destination and make sure the directory exists.");
+//  }
+  if (map_file == std::string(""))
+  {
+    ROS_ERROR("specify a simplemap file destination and make sure the directory exists.");
+  }
+
+  map_file_.open(map_file, std::fstream::out);
+  if (!map_file_.is_open()){
+    ROS_ERROR("cannot open file: %s, make sure the directory exists.", map_file.c_str());
+  }
+
+  sub_map_ = n_.subscribe("map", 1, &ObjectListener::callbackMap, this);
+  sub_object_detections_ = n_.subscribe("object_detections", 1, &ObjectListener::callbackObjectDetections, this);
+  metric_map_ = boost::make_shared<CMultiMetricMap>();
+  metric_map_->m_gridMaps.push_back(new mrpt::maps::COccupancyGridMap2D());
+  //metric_map_->m_beaconMap.push_back(new mrpt::maps::CBeaconMap());
+}
+
+void callbackMap(nav_msgs::OccupancyGrid &_msg)
+{
+  //mrpt_bridge::convert()
+}
+
+void callbackObjectDetections(tuw_object_msgs::ObjectDetection &_msg)
+{
+
+}
+
+int main(int argc, char **argv)
+{
+  ros::init(argc, argv, "object_listener_node");
+  ros::NodeHandle nh;
+
+  ROS_INFO("Hello world!");
+}
